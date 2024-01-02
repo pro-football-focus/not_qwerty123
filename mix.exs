63277e788ec0b854ce8bacbfe3d3a5c46fa2a265
Ian Perks <ian@pff.com>
2024-01-02 16:55:50 +0000

    Fix new compile warnings and upgrade deps

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule NotQwerty123.Mixfile do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "NotQwerty123",
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: [:yecc] ++ Mix.compilers(),
       package: package(),
       deps: deps(),
       docs: docs()
